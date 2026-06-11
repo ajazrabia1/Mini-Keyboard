71315667c69f9096343e2fc6a21be966f3d84881
ajazrabia1 <ajazrabia1@gmail.com>
2026-06-10 21:53:02 -0500

    Update main.py

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,31 +1,30 @@
-#This will be the main file for the minikeyboard code
+# main.py
 import board
 
-from kmk.kmk_keyboard import keyboard
+from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 
-keyboard= KMKKeyboard()
+keyboard = KMKKeyboard()
 
-# Four direct-wired buttons
-keyboard.direct_pins=[
-    board.D0, #SW1
-    board.D1, #SW2
-    board.D2, #SW3
-    board.D3, #SW4
-
+# Buttons wired to D0-D3 and GND
+keyboard.direct_pins = [
+    board.D0,  # SW1
+    board.D1,  # SW2
+    board.D2,  # SW3
+    board.D3,  # SW4
 ]
 
-keyboard.coord_mapping=(0,1,2,3)
+keyboard.coord_mapping = (0, 1, 2, 3)
 
-# key assignments 
-keyboard.keymap=[
+# Key assignments
+keyboard.keymap = [
     [
-        KC.F13,
-        KC.F14,
-        KC.F15,
-        KC.F16,
+        KC.COPY,
+        KC.PASTE,
+        KC.CUT,
+        KC.UNDO,
     ]
 ]
 
-if __name__=="__main__":
-  keyboard.go()
+if __name__ == "__main__":
+    keyboard.go()
